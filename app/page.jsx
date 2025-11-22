1d435ec10c75560af55f8c7cb4451af18683c5e0
akram-abid <a.abid44778@gmail.com>
2025-11-22 17:03:05 +0100

    Make the servers's container scrollable

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
+import Contacts from "@/components/Contacts";
 import Navrow from "../components/navrow";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-[80px_300px_1fr] h-screen">
+    <div className="grid grid-cols-[70px_300px_1fr] h-screen">
       <Navrow/>
-      <div className="bg-surface-alt">contacts</div>
+      <Contacts />
       <div className="bg-blue-900">conversation</div>
     </div>
   );
--- components/navrow.jsx
+++ components/navrow.jsx
@@ -1,4 +1,9 @@
-import { faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAdd,
+  faGear,
+  faRightFromBracket,
+  faScrewdriverWrench,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import { FloatingDockDemo } from './ui/FloatingDockDemo';
@@ -8,33 +13,53 @@ import ThemeSwitcherButton from './ThemeSwitcher';
 
 const Navrow = () => {
   return (
-    <div className="bg-mate flex flex-col items-center gap-2 py-2">
-      <Image
-        src="/logo.png"
-        alt="logo"
-        width={33}
-        height={33}
-        className="mb-4 pt-1"
-      />
-      <div className="rounded-full bg-primary p-1.5">
-        <div className="rounded-full">
-          <Image
-            src="/profilepic.jpeg"
-            alt="profile"
-            width={50}
-            height={50}
-            className="rounded-full object-cover"
-          />
+    <div className="bg-mate flex flex-col items-center gap-2 p-2 h-screen">
+      <div className='flex flex-col justify-center items-center gap-3'>
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={33}
+          height={33}
+          className="mb-2 pt-1"
+        />
+        <Image
+          src="/profilepic.jpeg"
+          alt="profile"
+          width={45}
+          height={45}
+          className="rounded-full object-cover"
+        />
+      </div>
+
+      <div className="w-8 h-px bg-accent/30 my-2"></div>
+
+      <div className="flex flex-col justify-center items-center gap-3 flex-1 min-h-0">
+        <div className="flex-1 w-full rounded-2xl p-2 flex flex-col gap-4 overflow-y-auto hover:bg-mate-light  transition-all duration-300 min-h-0 scrollbar-hide">
+          <div className="w-[40px] h-[40px] bg-amber-400 rounded-[8px] flex-shrink-0"></div>
+          <div className="w-[40px] h-[40px] bg-amber-400 rounded-[8px] flex-shrink-0"></div>
+          <div className="w-[40px] h-[40px] bg-amber-400 rounded-[8px] flex-shrink-0"></div>
+          <div className="w-[40px] h-[40px] bg-amber-400 rounded-[8px] flex-shrink-0"></div>
+          <div className="w-[40px] h-[40px] bg-amber-400 rounded-[8px] flex-shrink-0"></div>
+          <div className="w-[40px] h-[40px] bg-amber-400 rounded-[8px] flex-shrink-0"></div>
+          <div className="w-[40px] h-[40px] bg-amber-400 rounded-[8px] flex-shrink-0"></div>
+          <div className="w-[40px] h-[40px] bg-amber-400 rounded-[8px] flex-shrink-0"></div>
+          <div className="w-[40px] h-[40px] bg-amber-400 rounded-[8px] flex-shrink-0"></div>
+        </div>
+        <div className="w-[40px] h-[40px] bg-mate-light rounded-[8px] flex items-center justify-center flex-shrink-0">
+          <FontAwesomeIcon icon={faAdd} className="text-accent w-4" />
         </div>
       </div>
-      <div className="flex-1 bg-primary w-full rounded-2xl p-2 flex flex-col gap-6 overflow-hidden">
-        <div className="w-full h-[60px] bg-amber-400 rounded-2xl"></div>
-        <div className="w-full h-[60px] bg-amber-400 rounded-2xl"></div>
-        <div className="w-full h-[60px] bg-amber-400 rounded-2xl"></div>
-        <div className="w-full h-[60px] bg-amber-400 rounded-2xl"></div>
+
+      <div className="w-8 h-px bg-accent/30 my-2"></div>
+
+      <div className="flex flex-col justify-center items-center gap-3 ">
+        <FontAwesomeIcon icon={faGear} className="w-6 text-accent" />
+        <ThemeSwitcherButton />
+        <FontAwesomeIcon
+          icon={faRightFromBracket}
+          className="w-6 text-red-500 mb-2"
+        />
       </div>
-      <FontAwesomeIcon icon={faScrewdriverWrench} className='w-7 text-accent' />
-      <ThemeSwitcherButton />
     </div>
   );
 };
